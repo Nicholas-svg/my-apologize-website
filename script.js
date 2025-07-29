09bfd3521a6df49b589d9b84bbec6648b6402e31
Nicholas-svg <nicholassatria01@gmail.com>
2025-07-29 23:15:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Serius?",
+    "Seriuss?🥺🥺",
+    "Kamu yakin??",
+    "Maafinn sayaanggg",
+    "Pleaseee",
+    "Kalo ngga, aku sedihh :((",
+    "Sedih bangettt",
+    "Banget banget bangettt",
+    "Huhuhuhu :((",
+    "🥺🥺🥺🥺🥺"
 ];
 
 let messageIndex = 0;
